595da5660576688ad3340f40e68e02ef0a608ab9
Saltssaumure <themes@saltssaumure.net>
2023-10-10 02:29:43 +0100

    Setup for rerelease

---
+++ scss-compile.config.js
@@ -0,0 +1,9 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+    meta: {
+        name: "EmojiPicker" /* The official name of your theme. */,
+        scss: "pick" /* The name of your base scss file. */,
+        repo: "EmojiPicker" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
+    }
+};
